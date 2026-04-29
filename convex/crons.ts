51c2aa9f5ffc641e7fb8071963151caa5244b4cf
nspage <nicolas.s.page@gmail.com>
2026-04-29 17:10:36 +0200

    deepfreeze

--- convex/crons.ts
+++ convex/crons.ts
@@ -3,10 +3,13 @@ import { api } from "./_generated/api";
 
 const crons = cronJobs();
 
+/* 
+DEEP FREEZE: All automated polling is currently paused.
 crons.interval(
   "poll-allium-whale-activity",
   { minutes: 10 },
   api.allium.pollWhaleActivity,
 );
+*/
 
 export default crons;
